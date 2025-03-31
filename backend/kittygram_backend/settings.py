fe4157a343fb06c06b66a50fc6ab5f3ab1f19847
Fedorenko Ilia <fedorenkoilia@gmail.com>
2025-03-31 12:35:14 +0200

    1 fix for rewiver

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
